4ead2111bf1bdb8f87c29060b0275406e6100399
sebastian-reyes <sebastianreyes2105@gmail.com>
2023-07-31 11:44:07 -0500

    configuration failed, try to fix later

--- src/config.js
+++ src/config.js
@@ -6,5 +6,5 @@ export default {
   host: process.env.HOST || "",
   database: process.env.DATABASE || "",
   user: process.env.USER || "",
-  password: process.env.PASSOWRD,
+  password: process.env.PASSOWRD || "",
 };
