b246ace556933a3841a4d5ca317d9625de5d1fdb
sebastian-reyes <sebastianreyes2105@gmail.com>
2023-08-01 11:56:23 -0500

    configuring .env and adding example

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const { config } = require("dotenv");
+import 'dotenv/config';
 
 config();
 
--- src/database/database.js
+++ src/database/database.js
@@ -1,20 +1,12 @@
 import mysql from "promise-mysql";
+require('dotenv').config();
 
-/*
 const connection = mysql.createConnection({
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
-});
-*/
-const connection = mysql.createConnection({
-    host: "localhost",
-    database: "world_database",
-    user: "root",
-    password: "12345",
-    port: "3306",
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
   });
 
 const getConnection = () => {
