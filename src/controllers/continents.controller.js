a510a218a0ecafcc434e7bee667406361ad5ffd6
sebastian-reyes <sebastianreyes2105@gmail.com>
2023-08-01 23:13:37 -0500

    getContinent(id) OK

--- src/controllers/continents.controller.js
+++ src/controllers/continents.controller.js
@@ -4,18 +4,44 @@ const countrieModel = require("../../models").countrie;
 const getContinents = async (req, res) => {
   continentModel
     .findAll({
-      order:[['id']],
-      include: [{ model: countrieModel }],
+      order: [["id"]],
+      include: [
+        {
+          model: countrieModel,
+          attributes: { exclude: ["continent_id"] },
+        },
+      ],
     })
     .then((data) => {
       res.json(data);
     })
     .catch((error) => {
-      res.status(500)
+      res.status(500);
+      res.json({ error: error });
+    });
+};
+
+const getContinent = async (req, res) => {
+  const { id } = req.params;
+  continentModel
+    .findByPk(id, {
+      include: [
+        { 
+          model: countrieModel, 
+          attributes: { exclude: ["continent_id"] } 
+        },
+      ],
+    })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      res.status(500);
       res.json({ error: error });
     });
 };
 
 export const methods = {
   getContinents,
+  getContinent,
 };
--- src/routes/continents.routes.js
+++ src/routes/continents.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { methods as ContinentController } from "../controllers/continents.controller";
 const router = Router();
 
-router.get("/",ContinentController.getContinents);
+router.get("/", ContinentController.getContinents);
+router.get("/:id", ContinentController.getContinent);
 
 export default router;
